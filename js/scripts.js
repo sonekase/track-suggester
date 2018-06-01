24e8f782463fa0738f0bca05b7e1d5f9c8d77b44
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-06-01 15:58:27 -0700

    Enter branch codes to produce result on radio survey, also enter scroll animation from top of page to bottom.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
-var developmentPath = $("#development-focus").val();
-$(".clickable").click(function() {
-  $("#survey-showing").toggle();
-  $("#survey-hidden").toggle();
+  $("button#scroll").click(function(){
+  $('html, body').animate({scrollTop:$(document).height()}, 'slow');  
   });
+  $(".clickable").click(function() {
+    $("#survey-showing").toggle();
+    $("#survey-hidden").toggle();
+  });
+     $("#program-survey").submit(function(event) {
+        event.preventDefault();
+
+    var developmentPath = $("#development-focus").val();
+    var workSpace = $("input:radio[name=work-space]:checked").val();
+    var codeElement = $("input:radio[name=coding-type]:checked").val();
+    var progLanguage = $("input:radio[name=languages]:checked").val();
+    var buildType = $("input:radio[name=create]:checked").val();
+    var openErollment = $("input:radio[name=enrollment]:checked").val();
+    var trackSuggester = "CSS/React";
+
+    if(workSpace ==="startups" && codeElement==="design"){
+    trackSuggester = "C#/.NET";
+    }
+    if(workSpace==="enterprises" && codeElement==="functions"){
+    trackSuggester = "CSS/React";
+    }
+    if(workSpace ==="freelance" && codeElement==="both" && buildType ==="websites"){
+      trackSuggester = "CSS/React";
+    }
+    if(openErollment ==="improvement" && workSpace ==="startups" && buildType ==="applications") {
+      trackSuggester = "Ruby/Rails";
+    }
+    if(progLanguage ==="C#/.Net" && codeElement ==="design" && openErollment ==="education") {
+      trackSuggester = "C#/.Net";
+    }
+
+  $("#result").text(trackSuggester);
+  });
+
 });
