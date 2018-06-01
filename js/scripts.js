5f84856a5cb4b1357bbf73b94ef11212a8f1144a
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-06-01 12:54:55 -0700

    Add .clickable to hide and show survey.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 var developmentPath = $("#development-focus").val();
+$(".clickable").click(function() {
+  $("#survey-showing").toggle();
+  $("#survey-hidden").toggle();
+  });
 });
